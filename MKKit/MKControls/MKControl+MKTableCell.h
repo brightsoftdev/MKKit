b460a9d2506951fbe4e7d021dea8369467cc671e
Matthew King <matt62king@gmail.com>
2012-04-26 22:25:23 -0400

    Minor Updates
    Changed notifications from table view pull down controller.
    
    Moved MKControl+MKTableCell to its own file

---
+++ MKKit/MKControls/MKControl+MKTableCell.h
@@ -0,0 +1,13 @@
+//
+//  MKControl+MKTableCell.h
+//  MKKit
+//
+//  Created by Matt King on 4/26/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "MKControl.h"
+
+@interface MKControl (MKTableCell)
+
+@end
---
+++ MKKit/MKControls/MKControl+MKTableCell.m
@@ -0,0 +1,13 @@
+//
+//  MKControl+MKTableCell.m
+//  MKKit
+//
+//  Created by Matt King on 4/26/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "MKControl+MKTableCell.h"
+
+@implementation MKControl (MKTableCell)
+
+@end
